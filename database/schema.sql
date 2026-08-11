7f96882af0206cf3c9c104fa9f0b606a07d72b4e
Diyahaja <122991471+Diyahaja@users.noreply.github.com>
2026-08-11 08:15:32 +0700

    Update schema.sql

--- database/schema.sql
+++ database/schema.sql
@@ -1,4 +1,4 @@
-kalCREATE TABLE IF NOT EXISTS users (
+CREATE TABLE IF NOT EXISTS users (
     id          SERIAL PRIMARY KEY,
     username    VARCHAR(50)  NOT NULL UNIQUE,
     email       VARCHAR(100) NOT NULL UNIQUE,
